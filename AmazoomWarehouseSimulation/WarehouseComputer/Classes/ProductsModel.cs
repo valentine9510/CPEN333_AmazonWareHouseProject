b1cd626cffff69ddf89d7badde1fe037e87ff5b2
jpark326 <park.joseph@outlook.com>
2021-11-21 12:47:50 -0800

    forgot to also add product class

---
+++ AmazoomWarehouseSimulation/WarehouseComputer/Classes/ProductsModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetApplication.Models
+{
+    public class ProductsModel
+    {
+        public double weight_ { get; set; }
+        public string name_ { get; set; }
+        public ProductsModel(double weight, string name)
+        {
+            weight_ = weight;
+            name_ = name;
+        }
+
+    }
+    
+}
