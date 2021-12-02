2cdfb2aae536288b6bbc6a4412db58bf840f7542
Valentine Ssebuyungo <vssebuyungo@gmail.com>
2021-12-02 14:24:32 -0800

    Front end and back end are connected!

--- AmazoomWarehouseSimulation/AmazoomWebServer/Controllers/HomeController.cs
+++ AmazoomWarehouseSimulation/AmazoomWebServer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using AmazoomWebServer.Models;
+using AmazoomClassLibrary;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,6 +12,8 @@ namespace AmazoomWebServer.Controllers
 {
     public class HomeController : Controller
     {
+        static ProductInventory tempInventory = new ProductInventory();
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -30,10 +33,80 @@ namespace AmazoomWebServer.Controllers
 
         public IActionResult Orderspage()
         {
-            ProductInventory.LoadRandomObjects();
-            List<ProductFront> availableProducts = ProductInventory.getInventory();
+            
 
-            return View(availableProducts);
+            return View(tempInventory);
+        }
+
+        [HttpPost]
+        public ActionResult AddToOrder(int qty, string currentItemName, int currentItemNum)
+        {
+            //Product tempProduct = currentItem;
+            if (qty > currentItemNum || qty < 0) /*Avoid negative values*/
+            {
+                return View("Orderspage", tempInventory);
+            }
+            else
+            {
+                /*Reduce on the number of products in inventory*/
+                foreach(var item in ProductInventory.availableProducts)
+                {
+                    if (item.ProductName == currentItemName)
+                    {
+                        item.NumOfProduct-= qty;
+                    }
+                }
+                ProductInventory.currentOrder.AddProduct(new Product(currentItemName, qty)); //Add to current order
+            }
+            return View("Orderspage",tempInventory);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrderToQueue()
+        {
+            if (ProductInventory.currentOrder.Products.Count >= 1) /* Only add if it has at least a product */
+            {
+                Order tempOrder = ProductInventory.currentOrder;
+                ProductInventory.uncompletedOrders.Enqueue(tempOrder);
+
+                /* Reset order, add one to ID for next Order */
+                ProductInventory.currentOrder = new Order(ProductInventory.currentOrder.OrderID + 1);
+            }
+            
+            return View("Orderspage", tempInventory);
+        }
+
+        [HttpPost]
+        public ActionResult ClearOrder()
+        {
+            /* Clear  order*/
+            if(ProductInventory.currentOrder.Products != null)
+            {
+                ProductInventory.currentOrder.Products.Clear();
+            }
+            
+
+            return View("Orderspage", tempInventory);
+        }
+
+        [HttpPost]
+        public ActionResult ClearsOrdersInProgress()
+        {
+            if (ProductInventory.uncompletedOrders.Count >= 1)
+            {
+                ProductInventory.uncompletedOrders.Clear();
+            }
+            return View("Orderspage", tempInventory);
+        }
+
+        [HttpPost]
+        public ActionResult ClearsCompletedOrder()
+        {
+            if (ProductInventory.completedOrders.Count >= 1)
+            {
+                ProductInventory.completedOrders.Clear();
+            }
+            return View("Orderspage", tempInventory);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- AmazoomWarehouseSimulation/AmazoomWebServer/Models/ProductFrontEnd.cs
+++ AmazoomWarehouseSimulation/AmazoomWebServer/Models/ProductFrontEnd.cs
@@ -1,60 +1,114 @@
 ﻿using AmazoomClassLibrary;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AmazoomWebServer.Models
 {
-    //public class ProductFrontEnd : Product
-    //{
-    //    public int ProductStock;
-
-    //    // public image
-
-    //    //public url
-    //}
-
-    public class ProductFront
+    public class ProductFrontEnd : Product
     {
-        public string name { get; set; }
+        public int ProductStock;
 
-        public int numOfProduct { get; set; }
+        // public image
 
-        public int weight { get; set; }
-
-        public ProductFront(string name, int num, int weight)
-        {
-            this.name = name;
-            numOfProduct = num;
-            this.weight = weight;
-        }
+        //public url
     }
 
-    public static class ProductInventory
+
+    public class ProductInventory
     {
-        static List<ProductFront> availableProducts = new List<ProductFront>();
-        
-        static public void LoadRandomObjects()
+        static public List<Product> availableProducts  { get; set; }
+        static public Order currentOrder { get; set; }
+
+        public Product currentProduct { get; set; }
+        static public Queue<Order> uncompletedOrders { get; set; }
+        static public Queue<Order> completedOrders { get; set; }
+
+        public ProductInventory()
+        {
+            availableProducts = new List<Product>();
+            uncompletedOrders = new Queue<Order>();
+            completedOrders = new Queue<Order>();
+            currentOrder = new Order(1);
+            this.LoadRandomObjects();
+        }
+        public void LoadRandomObjects()
         {
             var rand = new Random();
-            availableProducts.Add(new ProductFront("Apple", rand.Next(10,1000), rand.Next(100, 5000)));
-            availableProducts.Add(new ProductFront("Beer", rand.Next(10, 1000), rand.Next(100, 5000)));
-            availableProducts.Add(new ProductFront("Orange", rand.Next(10, 1000), rand.Next(100, 5000)));
-            availableProducts.Add(new ProductFront("Shirt", rand.Next(10, 1000), rand.Next(100, 5000)));
-            availableProducts.Add(new ProductFront("Playstation", rand.Next(10, 1000), rand.Next(100, 5000)));
-            availableProducts.Add(new ProductFront("Laptop", rand.Next(10, 1000), rand.Next(100, 5000)));
-            availableProducts.Add(new ProductFront("Nike Air", rand.Next(10, 1000), rand.Next(100, 5000)));
+            availableProducts.Add(new Product("Apple", rand.Next(10,1000)));
+            availableProducts.Add(new Product("Beer", rand.Next(10, 1000)));
+            availableProducts.Add(new Product("Orange", rand.Next(10, 1000)));
+            availableProducts.Add(new Product("Shirt", rand.Next(10, 1000)));
+            availableProducts.Add(new Product("Playstation", rand.Next(10, 1000)));
+            availableProducts.Add(new Product("Laptop", rand.Next(10, 1000)));
+            availableProducts.Add(new Product("Nike-Air", rand.Next(10, 1000)));
+
+            //Populate completed orders
+            Order temp = new Order(rand.Next(1, 1000));
+            temp.AddProduct(new Product("Apple", rand.Next(1,10)));
+            temp.AddProduct(new Product("Toaster", rand.Next(1, 10)));
+
+            Order temp2 = new Order(rand.Next(1, 1000));
+            temp2.AddProduct(new Product("Shirt", rand.Next(1, 10)));
+            temp2.AddProduct(new Product("Beer", rand.Next(1, 10)));
+
+            completedOrders.Enqueue(temp);
+            completedOrders.Enqueue(temp2);
+
+
+            //Populate uncompleted orders
+            temp.OrderID = 12;
+            uncompletedOrders.Enqueue(temp);
+            temp2.OrderID = 43;
+            temp2.AddProduct(new Product("Toaster", rand.Next(1, 10)));
+            uncompletedOrders.Enqueue(temp2);
+
+            Order temp3 = new Order(rand.Next(1, 1000));
+            temp3.AddProduct(new Product("Orange", rand.Next(1, 10)));
+            temp3.AddProduct(new Product("Nike-Air", rand.Next(1, 10)));
+
+            Order temp4 = new Order(rand.Next(1, 1000));
+            temp4.AddProduct(new Product("Beer", rand.Next(1, 10)));
+            temp4.AddProduct(new Product("Apple", rand.Next(1, 10)));
+            temp4.AddProduct(new Product("Bag", rand.Next(1, 10)));
+
+            completedOrders.Enqueue(temp3);
+            uncompletedOrders.Enqueue(temp4);
+
+            //Add to current order
+            currentOrder.AddProduct(new Product("Shirt", rand.Next(1, 10)));
+            currentOrder.AddProduct(new Product("Apples", rand.Next(1, 10)));
+            currentOrder.AddProduct(new Product("Laptop", rand.Next(1, 10)));
+            currentOrder.AddProduct(new Product("Orange", rand.Next(1, 10)));
+            currentOrder.AddProduct(new Product("Beer", rand.Next(1, 10)));
+
         }
 
-        static public void clearInventory()
+        public void clearInventory()
         {
             availableProducts.Clear();
         }
 
-        static public List<ProductFront> getInventory()
+        public List<Product> getInventory()
         {
             return availableProducts;
+        }
+
+        public Queue<Order> getUncompletedOrdersQueue()
+        {
+            return uncompletedOrders;
+        }
+
+        public Queue<Order> getCompletedOrdersQueue()
+        {
+            return completedOrders;
+        }
+
+        public Order getCurrentOrder()
+        {
+            return currentOrder;
         }
 
     }
